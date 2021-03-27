6e12d05ce4f09e0bbd826d9da770582483db0952
Rui Coelho <rui.coelho@miniclip.com>
2021-03-27 14:55:19 +0000

    Add vimac 0.3.6

---
+++ Casks/vimac.rb
@@ -0,0 +1,15 @@
+cask "vimac" do
+  version "0.3.6"
+  sha256 "e03ed4f0e86082abd7b12210177a3d40ab87a0319bcb326229ed8d3b133fe409"
+
+  url "https://github.com/dexterleng/vimac/releases/download/v#{version}/vimac-v#{version}.zip"
+  name "vimac"
+  desc "Productive macOS keyboard-driven navigation"
+  homepage "https://github.com/dexterleng/vimac"
+
+  livecheck do
+    skip "No version information available"
+  end
+
+  app "Vimac.app"
+end
